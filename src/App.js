420af66303a8fef809948fd3a52206552d10b22f
Mauro Emmanuel Rambo <mauroemmanuel22@gmail.com>
2020-12-04 22:54:46 -0300

    Material ui y paleta de colores

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          El Clásico <code><em>Hola Mundo</em></code> para empezar la app.
-        </p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
