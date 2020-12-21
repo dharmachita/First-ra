727e3f7d57f517cb623cc856f299fb0db2250b58
Mauro Emmanuel Rambo <mauroemmanuel22@gmail.com>
2020-12-20 21:35:15 -0300

    Se incorpora Promises y se emula el consumo de datos desde API

--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,9 +1,11 @@
 import Item from '../components/Item'
-import React from 'react';
+import {useEffect,useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import productos from '../utils/productos.json';
+import api from '../utils/productos.json';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
+//Estilos
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
@@ -13,22 +15,65 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function SpacingGrid() {
+export default function ItemListContainer() {
   const classes = useStyles();
+  const [productos,setProductos] = useState([]);
+
+
+  //Emulación de busqueda de datos en API
+  const buscarEnApi = new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(api);
+      },2000)
+  })
+
+  //Función asíncrona para capturar la llamada a la API 
+  //Usando async/await
+  /*
+  const getProducto = async ()=>{
+    try{
+      const result = await buscarEnApi;
+      setProductos(result);
+      console.log(productos);
+    }catch(err){
+      console.log(err);
+    }
+  }
+  */
+
+
+  //useEffect para ejecutar la llamada cuando se monta el componente
+  useEffect(()=>{
+    buscarEnApi
+    .then(result=>{
+      setProductos(result);
+    })
+    .catch(err=>{
+      console.error(err);
+    })
+    //getProducto();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
+
 
   return (
     <Grid container className={classes.root} spacing={2}>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={4}>
-          {productos.map((producto) => (
-            <Item 
-                title={producto.titulo}
-                img={producto.url}
-                price={producto.precio}
-                alt={producto.alt} 
-                stock={producto.stock}
-            />
-          ))}
+          { !productos.length?
+            <CircularProgress className="loader" color="secondary" />:             
+            productos.map((producto, index) => (
+              <Item 
+                  key={index}
+                  title={producto.titulo}
+                  img={producto.url}
+                  price={producto.precio}
+                  alt={producto.alt} 
+                  stock={producto.stock}
+              />
+            ))
+          }
         </Grid>
       </Grid>
     </Grid>
--- src/global/Categories.jsx
+++ src/global/Categories.jsx
@@ -6,8 +6,8 @@ import Grid from '@material-ui/core/Grid';
 export default function Categories(){
     return (
         <Grid container spacing={0} className='medium'>
-            {categories.map(function(category){
-                return (<Grid item xs><a className="cat-item" href={category.url}>{category.cat}</a></Grid>);    
+            {categories.map((category,index)=>{
+                return (<Grid key={index} item xs><a className="cat-item" href={category.url}>{category.cat}</a></Grid>);    
                 })
             }    
         </Grid>
